86b8b23d8ac4baa597323561731f881ba24b7be8
66minmin <christian@omg.games>
2023-09-20 19:52:38 -0500

    test graffiti package

--- src/index.server.ts
+++ src/index.server.ts
@@ -1,6 +1,8 @@
 const toolbar = plugin.CreateToolbar("MyToolbar");
 const button = toolbar.CreateButton("MyButton", "", "");
 
+import { makeHello } from "@rbxts/graffiti";
+
 button.Click.Connect(() => {
-	print("Button clicked!");
+	print(makeHello("Graffiti"));
 });
